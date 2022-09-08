19484fd38f5a0d6957601f4b746926291c7f8b78
Andrey <megainvest@yandex.ru>
2022-09-08 19:28:13 +0300

    Задача 32

--- Program.cs
+++ Program.cs
@@ -3,14 +3,55 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
 // 6, 1, 33 -> [6, 1, 33]
-int [] arrey = new int [8];
-int index = 0;
-int length = arrey.Length;
-while (index < length)
-{
-    arrey[index] = new Random().Next(20);
-    Console.Write(arrey[index] + " ");
-    index ++;
-}
+// int [] arrey = new int [8];
+// int index = 0;
+// int length = arrey.Length;
+// while (index < length)
+// {
+//     arrey[index] = new Random().Next(20);
+//     Console.Write(arrey[index] + " ");
+//     index ++;
+// }
+
+//Задачи с семинара
+
+// int [] array = new int[12];
+// int resPositive = 0;
+// int resNegative = 0;
+// int index = 0;
+// int length = array.Length;
+// while (index < length)
+// {
+//     array[index] = new Random().Next(-9, 10);
+//     Console.Write(array[index] + "\t");
+//     if (array[index] > 0) resPositive +=array[index];
+//     else resNegative += array[index];
+//     index++;
+// }
+// Console.WriteLine();
+// Console.WriteLine($"Сумма положительных чисел: {resPositive}, отрицательных: {resNegative}");
 
 
+// Задача 32: Напишите программу замена элементов массива: 
+//положительные элементы замените на соответствующие отрицательные, и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+// int[] array = new int[8];
+// int index = 0;
+// int length = array.Length;
+// while (index < length)
+// {
+//     array[index] = new Random().Next(-9, 10);
+//     // array[index] = array[index] * (-1);
+//     Console.Write(array[index] + "\t");
+//     index++;
+// }
+// index = 0;
+// Console.WriteLine();
+// while (index < length)
+// {
+//     // array[index] = new Random().Next(-9, 10);
+//     array[index] = array[index] * (-1);
+//     Console.Write(array[index] + "\t");
+//     index++;
+// }
