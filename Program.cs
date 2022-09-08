56c0d347e42e38b0b64e79c5da1fb863578a4d7f
Andrey <megainvest@yandex.ru>
2022-09-08 20:48:45 +0300

    Задача 33

--- Program.cs
+++ Program.cs
@@ -55,3 +55,38 @@
 //     Console.Write(array[index] + "\t");
 //     index++;
 // }
+
+// Задача 33: Задайте массив. Напишите программу, которая определяет, 
+// присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] array = new int[8];
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+bool flag = false;
+int length = array.Length;
+while (index < length)
+{
+    array[index] = new Random().Next(-9, 10);
+    // array[index] = array[index] * (-1);
+    Console.Write(array[index] + "\t");
+    if (array[index] == number)
+    {
+        flag = true;
+        break;
+    }
+    index++;
+}
+if(flag) Console.WriteLine($"Указанное число '{number}' в массиве присутствует");
+else Console.WriteLine($"Указанное число '{number}' в массиве отсутствует");
+
+// index = 0;
+// Console.WriteLine();
+// while (index < length)
+// {
+//     // array[index] = new Random().Next(-9, 10);
+//     array[index] = array[index] * (-1);
+//     Console.Write(array[index] + "\t");
+//     index++;
+// }
